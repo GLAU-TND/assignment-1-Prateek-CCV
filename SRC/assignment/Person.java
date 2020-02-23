e2aacef7ecfb67aa0f9d794bafae74e6a643cd1d
PRATEEK <prateekraj807@gmail.com>
2020-02-23 19:32:25 +0530

    created to string method

--- SRC/assignment/Person.java
+++ SRC/assignment/Person.java
@@ -1,10 +1,10 @@
 package assignment;
 
 public class Person {
-    private String firstname;
-    private String lastname;
+    private String firstname = "";
+    private String lastname = "";
     private String[] contactno;
-    private String email;
+    private String email = "";
 
     public Person(String firstname, String lastname, String[] contactno, String email) {
         this.firstname = firstname;
@@ -43,5 +43,20 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("-------- * -------- * -------- * --------\n");
+        sb.append("firstName=").append(firstname).append('\n');
+        sb.append("lastName=").append(lastname).append('\n');
+        sb.append("contactno=").append(contactno);
+
+        for (int i = 0; i < contactno.length; i++) {
+            sb.append((contactno[i] != null && i != contactno.length ? contactno[i] : ""));
+
+        }
+        sb.append("\nemail='").append(email).append('\n');
+        sb.append("-------- * -------- * -------- * --------");
+        return sb.toString();
     }
 }
